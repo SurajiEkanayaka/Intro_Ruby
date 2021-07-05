7dac311d793e86e60b2a60f523cf809dbd364a65
SurajiEkanayaka <surajiekanayaka4@gmail.com>
2021-07-05 19:56:28 +0530

    Introduction

---
+++ test1.rb
@@ -0,0 +1,68 @@
+#Run code - ruby test(project_name).rb
+
+#for print
+puts "Hello"  #puts - with line break
+puts "Everyone"
+print "Hi"  #print - no line break
+print "Hi2"
+print "Hi3"
+puts "!"
+
+#single line comment
+=begin
+multi line comment
+=end
+
+=begin
+output
+Hello
+Everyone
+Hi1Hi2Hi3!
+=end
+
+#VARIABLES
+=begin
+variables are case sensitive and should start with lowercase word
+ underscore can be used.
+    eg : my_name  
+
+=end
+
+name = "Suraji" #string
+age = 21 #Integer
+gpa = 4.9 #Decimal
+is_true=true #Boolean true or false
+
+
+puts "Your name : #{name}"
+puts "Your age : #{age}"
+puts "Your gpa : #{gpa}"
+
+puts 2.14.to_i  #to integer (convert to integer)
+puts 2.to_f  #to float (convert to float)
+puts 2.5.to_s #to string
+ 
+puts 10 + "50".to_i
+puts 100+ "50.67".to_f
+
+=begin
+output
+Your name : Suraji
+Your age : 21
+Your gpa : 4.9
+2
+2.0
+2.5
+60
+150.67000000000002
+=end
+
+
+#STRINGS
+
+
+
+
+
+
+
